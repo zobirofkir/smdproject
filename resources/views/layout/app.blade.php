8c0e849a9c22397e113e5242e3902b586a89dea8
zobirofkir <zobirofkir19@gmail.com>
2024-10-30 23:20:56 +0100

    add search blog

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <title>{{config('app.name')}}</title>
 </head>
 <body class="bg-gray-100 scroll-smooth">
--- resources/views/screens/blogs.blade.php
+++ resources/views/screens/blogs.blade.php
@@ -1,4 +1,3 @@
-{{-- resources/views/blogs.blade.php --}}
 <x-app-layout>
     <div class="container mx-auto px-4 py-10">
         {{-- Blog Header --}}
@@ -8,6 +7,17 @@
 
         @include('components.categories')
 
+        <div class="flex justify-center items-center mb-10 px-4">
+            <div class="w-full max-w-md">
+                <form action="" class="flex items-center border border-gray-300 rounded-full overflow-hidden shadow-sm focus-within:ring-2 focus-within:ring-blue-500">
+                    <button type="submit" class="pl-4 pr-4 text-gray-500 hover:text-blue-500 focus:outline-none">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                    <input type="text" name="search" placeholder="Search" class="flex-grow px-4 py-2 text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-0">
+                </form>
+            </div>
+        </div>
+        
         <section class="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
             @foreach ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] as $blog)
                 <article class="bg-white border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition ease-in-out duration-300 px-6 py-4">
