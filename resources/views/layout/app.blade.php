32658b0e8b52889e5242cdbed693293e5c3c42f5
zobirofkir <zobirofkir19@gmail.com>
2024-10-30 07:04:09 +0100

    download tailwind css and create AppLayout

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css', 'resources/js/app.js')
+    <title>{{config('app.name')}}</title>
+</head>
+<body>
+    <div>
+        {{ $slot }}
+    </div>
+</body>
+</html>
