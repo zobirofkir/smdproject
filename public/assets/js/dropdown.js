1617c2237b7af1428941a45a21ad3456ef5b992a
zobirofkir <zobirofkir19@gmail.com>
2024-10-30 12:15:19 +0100

    remove comments from drop down

--- public/assets/js/dropdown.js
+++ public/assets/js/dropdown.js
@@ -3,10 +3,8 @@ const mobileMenu = document.getElementById('mobile-menu');
 
 menuToggle.addEventListener('click', () => {
     if (mobileMenu.style.maxHeight) {
-        // Close the menu
         mobileMenu.style.maxHeight = null; 
     } else {
-        // Open the menu
         mobileMenu.classList.remove('hidden'); 
         const height = mobileMenu.scrollHeight;
         mobileMenu.style.maxHeight = `${height}px`; 
